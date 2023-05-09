ef7a2f7eb8265f14664408be5221854ae81ce739
Ziad Abdalla <105611439+ziad223@users.noreply.github.com>
2023-05-09 15:32:16 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question :"  <iframe src='audios/dog.mp3'> ",
+    answer: "Dog",
+    options: [
+      "Dog",
+      "Cat",
+      "Cow",
+      "Donkey"
+    ]
+  },
+    {
+    numb: 2,
+    question: "<iframe src='audios/dolphin.mp3'>",
+    answer: "Dolphin",
+    options: [
+      "Cat",
+      "Giraffe",
+      "Dolphin",
+      "Snake"
+    ]
+  },
+    {
+    numb: 3,
+    question: "<iframe src='audios/griffe.mp3'>",
+    answer: "Giraffe",
+    options: [
+      "Cat",
+      "Giraffe",
+      "Dolphin",
+      "Snake"
+    ]
+  },
+    {
+    numb: 4,
+    question: "<iframe src='audios/snake.mp3'>",
+    answer: "Snake",
+    options: [
+      "Cat",
+      "Giraffe",
+      "Dolphin",
+      "Snake"
+    ]
+  },
+    {
+    numb: 5,
+    question: "<iframe src='audios/monkey.mp3'>",
+    answer: "Monkey",
+    options: [
+      "Elephant",
+      "Giraffe",
+      "Donkey",
+      "Monkey"
+    ]
+  },
+];
